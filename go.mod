7f737ce50791e06ba2d5ee40427088c3b8c4504a
Elle Mouton <elle.mouton@gmail.com>
2021-02-21 09:14:21 +0200

    serve first image and cv

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ellemouton/thunder
 
 go 1.14
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gomarkdown/markdown v0.0.0-20210208175418-bda154fe17d8
+	github.com/gorilla/mux v1.8.0
+)
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gomarkdown/markdown"
 	"github.com/gorilla/mux"
 )
 
@@ -15,18 +16,24 @@ func main() {
 	r := newRouter()
 
 	fmt.Println("Serving on port 8080")
-	if err := http.ListenAndServe(":8080", r); err != nil {
-		panic(err)
-	}
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 func newRouter() *mux.Router {
 	r := mux.NewRouter()
+
 	r.HandleFunc("/", homeHandler).Methods("GET")
 	r.HandleFunc("/blog", blogHandler).Methods("GET")
 	r.HandleFunc("/gallery", galleryHandler).Methods("GET")
 	r.HandleFunc("/projects", projectsHandler).Methods("GET")
+	r.PathPrefix("/images").Handler(http.StripPrefix("/images", http.FileServer(http.Dir("assets/images"))))
+	r.HandleFunc("/cv", cvHandler).Methods("GET")
+
 	return r
+}
+
+func cvHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "assets/other/CV_Elle_Mouton.pdf")
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -36,8 +43,22 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type Content struct {
+	Text template.HTML
+}
+
 func blogHandler(w http.ResponseWriter, r *http.Request) {
-	err := templates.ExecuteTemplate(w, "blog.html", nil)
+	md := []byte("## markdown document\n" +
+		"![testing](/images/well.png)\n" +
+		"blah `blah` blah")
+
+	output := markdown.ToHTML(md, nil, nil)
+
+	c := Content{
+		Text: template.HTML(output),
+	}
+
+	err := templates.ExecuteTemplate(w, "blog.html", c)
 	if err != nil {
 		log.Println(err)
 	}
