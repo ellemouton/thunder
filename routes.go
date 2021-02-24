cd46c8df2ad66571f52b02e14d8166126e5c8661
Elle Mouton <elle.mouton@gmail.com>
2021-02-24 15:47:21 +0200

    assets/images: Add txRequestTracker diagram

--- routes.go
+++ routes.go
@@ -172,66 +172,6 @@ func (s *State) blogShowHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-/*
-func (s *State) blogShowHandler(w http.ResponseWriter, r *http.Request) {
-	ctx := context.Background()
-	vars := mux.Vars(r)
-
-	id, err := strconv.ParseInt(vars["id"], 10, 64)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	info, err := blogs_db.LookupInfo(ctx, s.GetDB(), id)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	content, err := blogs_db.LookupContent(ctx, s.GetDB(), info.ContentID)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	//extensions := parser.CommonExtensions | parser.HardLineBreak
-	extensions := parser.NoIntraEmphasis        // Ignore emphasis markers inside words
-	extensions |= parser.Tables                 // Parse tables
-	extensions |= parser.FencedCode             // Parse fenced code blocks
-	extensions |= parser.Autolink               // Detect embedded URLs that are not explicitly marked
-	extensions |= parser.Strikethrough          // Strikethrough text using ~~test~~
-	extensions |= parser.SpaceHeadings          // Be strict about prefix heading rules
-	extensions |= parser.HeadingIDs             // specify heading IDs  with {#id}
-	extensions |= parser.BackslashLineBreak     // Translate trailing backslashes into line breaks
-	extensions |= parser.DefinitionLists        // Parse definition lists
-	extensions |= parser.LaxHTMLBlocks          // more in HTMLBlock, less in HTMLSpan
-	extensions |= parser.NoEmptyLineBeforeBlock // no need for new line before a list
-	extensions |= parser.HardLineBreak
-	extensions |= parser.EmptyLinesBreakList
-	parser := parser.NewWithExtensions(extensions)
-	output := markdown.ToHTML([]byte(content.Text), parser, nil)
-
-	c := struct {
-		Name     string
-		Abstract string
-		Date     time.Time
-		Content  template.HTML
-	}{
-		Name:     info.Name,
-		Abstract: info.Description,
-		Date:     info.CreatedAt,
-		Content:  template.HTML(output),
-	}
-
-	err = templates.ExecuteTemplate(w, "blog.html", c)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-*/
-
 func (s *State) blogHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
 
